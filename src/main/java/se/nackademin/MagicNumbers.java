5714bf6bfb7fa4620a084fb02659f8be5e111fc8
Sarah Wimark <sarah_beeblebrox@live.se>
2016-03-31 18:46:05 +0200

    Ändringar i MagicNumers.java och lagt till klass Translator

--- src/main/java/se/nackademin/MagicNumbers.java
+++ src/main/java/se/nackademin/MagicNumbers.java
@@ -9,28 +9,59 @@ public class MagicNumbers {
     private int height;
 
     public int calculateA() {
+        int numberOfNames = name.length() - name.replaceAll(" ", "").length() + 1;
+        int result = age + numberOfNames;
+        while(result > 10){
+           result = result - 7;
+        }
         //TODO: calculate A
-        return 0;
+        return result;
     }
 
     public int calculateB() {
+        int numberOfCharacters = location.length();
+        int result = income + numberOfCharacters;
+        while(result > 10){
+           result = result - 7;
+        }
         //TODO: calculate B
-        return 0;
+        return result;
     }
 
     public int calculateC() {
+        int result = calculateA() + calculateB() - height;
+        while(result < 0){
+           result = result + 10;
+        }
         //TODO: calculate C
-        return 0;
+        return result;
     }
 
     public int calculateD() {
+        int result = calculateA();
+        if(result > 5){
+            result = result + calculateB();
+        }
+        else{
+            result = result + calculateC();
+        }
+        result = result - income;
+        while(result < 0){
+           result = result + 10;
+        }
         //TODO: calculate D
-        return 0;
+        return result;
     }
 
     public int calculateE() {
+        double result = (age * income) * income * height;
+        result = Math.sqrt(result);
+        while(result >= 10){
+            result = result/2;
+        }
+        result = Math.round(result);
         //TODO: calculate E
-        return 0;
+        return (int)result;
     }
 
     public void setName(String name) {
